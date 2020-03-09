344ad61bc7786e8ad01f7d74925a0d99eab87b19
Sebastian <Sebastian>
2020-03-08 22:10:10 -0500

    prueba succ

--- pruebaclick2.py
+++ pruebaclick2.py
@@ -1,11 +1,14 @@
 import os
+import time
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-os.environ["PATH"] += os.pathsep + r'~\';
+## para utilizar la siguiente linea se debe instalar geckodriver en la carpeta C:
+## geckodriver controla la apertura de las ventanas del navegador
+os.environ["PATH"] += os.pathsep + r'C:\geckodriver-v0.26.0-win64';
 cap = DesiredCapabilities().FIREFOX
 cap["marionette"] = True
 cap['handleAlerts'] = True
@@ -19,8 +22,12 @@ binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
 for i in range(1,3):
     firefox_profile.set_preference("browser.tabs.remote.autostart."+str(i+1), True)
     browser = webdriver.Firefox(capabilities=cap, firefox_binary=binary )
-    browser.get('https://google.com/')
+    browser.get('https://es.chaturbate.com/dalialatinporn')
     browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+    time.sleep(5)
     # Get button you are going to click by its id ( also you could us find_element_by_css_selector to get element by css selector)
-    #button_element = browser.find_element_by_id('close_entrance_terms')
+    button_element = browser.find_elements_by_link_text('I AGREE')
+    print (button_element)
     #button_element.click()
+    
+    
